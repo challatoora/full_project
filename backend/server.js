9e3174d3b73e5089a3795b217b2fcf96a6eef481
challatoor murali <challatoor murali.gmail.com>
2026-07-11 20:30:11 +0530

    full

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const app = require("./app");
+
+const connectMySQL = require("./config/mysql");
+const connectMongoDB = require("./config/mongodb");
+
+const PORT = process.env.PORT || 5000;
+
+async function startServer() {
+  try {
+    await connectMySQL();
+    await connectMongoDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
